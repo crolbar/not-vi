6e3fe1b7ebbc424df7dfb360493dbade6fca25ad
crolbar <crolbar@crolbar.com>
2024-08-29 20:52:03 +0300

    nix flake update

--- flake.nix
+++ flake.nix
@@ -1,27 +1,28 @@
 {
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            pkg-config
-            (rust-bin.stable.latest.default.override {
-              extensions = [ "rust-src" ];
-            })
-          ];
-        };
-      }
-    );
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [(import rust-overlay)];
+    };
+  in
+    with pkgs; {
+      devShells.${pkgs.system}.default = mkShell {
+        buildInputs = [
+          pkg-config
+
+          (rust-bin.stable.latest.default.override {
+            extensions = ["rust-src" "rust-analyzer"];
+          })
+        ];
+      };
+    };
 }
